278b1b0c5b481c6c116e65af45a5f8a64e90fedf
张超 <chao.zhang@net263.com>
2015-03-13 17:34:40 +0800

    add Testing content

---
+++ MagicImageView/src/com/robert/test/TestMain.java
@@ -0,0 +1,19 @@
+package com.robert.test;
+
+import com.robert.magicimageview.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestMain extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.test01);
+	}
+
+	
+
+}
